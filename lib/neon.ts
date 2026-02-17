9db9de9b38b38b051b4803897979f69f9f9c2cc8
x0bd <synthmancer@gmail.com>
2026-02-17 16:24:42 +0200

    fix neon crypto error

--- lib/neon.ts
+++ lib/neon.ts
@@ -3,8 +3,12 @@
  *
  * Set EXPO_PUBLIC_NEON_AUTH_URL and EXPO_PUBLIC_NEON_DATA_API_URL
  * in your .env file after creating a Neon project with Auth enabled.
+ *
+ * We import `expo-standard-web-crypto` once here to polyfill `global.crypto`
+ * in React Native so Neon Auth can use Web Crypto APIs safely.
  */
 import { createClient } from '@neondatabase/neon-js'
+import 'expo-standard-web-crypto'
 
 export const neonClient = createClient({
   auth: {
