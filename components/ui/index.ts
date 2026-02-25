16e7a081a9c6e7bddfeaa9a7d257504778aca809
x0bd <synthmancer@gmail.com>
2026-02-25 09:50:19 +0200

    reset

--- components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-// Export all UI components from a single entry point
-export { Button } from './button'
-export type { UmoButtonProps } from './button'
-
-export { Card, CardHeader, CardContent, CardFooter } from './card'
-
-export { Input } from './input'
-export type { UmoInputProps } from './input'
-
--- server/src/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { neon } from '@neondatabase/serverless'
-import 'dotenv/config'
-import { drizzle } from 'drizzle-orm/neon-http'
-import * as schema from './schema'
-
-const sql = neon(process.env.DATABASE_URL!)
-export const db = drizzle(sql, { schema })
