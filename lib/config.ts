abfb588b5de6f5a873988fa9eb095e775c4f3e1a
x0bd <synthmancer@gmail.com>
2026-03-04 10:15:11 +0200

    chore update add bill screen to use new hono server api for price conversion

--- lib/config.ts
+++ lib/config.ts
@@ -8,4 +8,4 @@
  * with your machine's local IP address, e.g. http://192.168.1.x:3001
  */
 export const API_URL =
-  (process.env.EXPO_PUBLIC_API_URL as string | undefined) ?? 'http://localhost:3001';
+  (process.env.EXPO_PUBLIC_API_URL as string | undefined) ?? 'http://192.168.1.245:3000';
